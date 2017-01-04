e71d274f616fd9ab67502aaa40e8721e8869efb4
Juli0GT <julio.gteran@gmail.com>
2017-01-04 18:10:57 +0000

    Test for win game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -21,4 +21,19 @@ describe Game do
       expect(game.opponent_shape).to eq :paper
     end
   end
+
+  context 'game ended' do
+    subject(:win_game) { game }
+    subject(:draw_game) { described_class.new(lose_options) }
+    subject(:lose_options) { described_class.new(lose_options) }
+
+    let(:lose_options) { {'player_name' => 'Julio', 'player_shape' => :scissors, 'opponent_shape' => :rock} }
+    let(:draw_options) { {'player_name' => 'Julio', 'player_shape' => :scissors, 'opponent_shape' => :scissors} }
+
+    describe '#win?' do
+      it 'returns true if player shape is scissors and opponent shape is paper' do
+        expect(win_turn.win?).to eq true
+      end
+    end
+  end
 end
