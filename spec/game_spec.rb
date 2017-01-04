4534cacb374a7d08f8b0591b51e368955a0f9d41
Juli0GT <julio.gteran@gmail.com>
2017-01-04 15:28:09 +0000

    Test failing for oponent shape

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,10 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new(player_name, player_shape) }
+  subject(:game) { described_class.new(player_name, player_shape, oponent_shape) }
   let(:player_name) { 'Julio' }
   let(:player_shape) { 'scissors' }
+  let(:oponent_shape) { 'paper' }
 
   describe '#player_name' do
     it 'has a player name' do
@@ -14,6 +15,12 @@ describe Game do
   describe '#player_shape' do
     it 'has a player shape' do
       expect(game.player_shape).to eq 'scissors'
+    end
+  end
+
+  describe '#oponent_shape' do
+    it 'has an oponent shape' do
+      expect(game.oponent_shape).to eq 'paper'
     end
   end
 end
