db74bf6fa93eb0016a8f1ec1c4c579c00249acc4
Juli0GT <julio.gteran@gmail.com>
2017-01-04 15:22:40 +0000

    Test for a player shape

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,10 +3,17 @@ require 'game'
 describe Game do
   subject(:game) { described_class.new(player_name) }
   let(:player_name) { 'Julio' }
+  let(:player_shape) { 'scissors' }
 
-  describe '#name' do
+  describe '#player_name' do
     it 'has a player name' do
       expect(game.player_name).to eq 'Julio'
+    end
+  end
+
+  describe '#player_shape' do
+    it 'has a player shape' do
+      expect(game.player_shape).to eq 'scissors'
     end
   end
 end
